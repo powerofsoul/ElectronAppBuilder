49812cefc022cb98d2067aabaaeaac5966111386
Florin Munteanu <florinmunteanu96@gmail.com>
2019-02-24 14:13:19 +0200

    Add script to create package

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,21 @@ module.exports = function(grunt) {
             {expand: true, cwd:"src/", src: ['**/*.html'], dest: 'dist/'},
           ],
         },
-      }
+      },
+      electron: {
+		macosBuild: {
+			options: {
+                    name: 'App Builder',
+                    dir: 'src',
+                    out: 'out',
+                    version: '0.0.1',
+                    platform: 'linux',
+                    arch: 'x64'
+			    }
+		    }
+	    }
     });
   
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-electron');
 };
