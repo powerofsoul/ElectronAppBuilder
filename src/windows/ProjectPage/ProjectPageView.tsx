fa5e8d302eb7401f192b1fc3bb6048b76dd3de6e
Florin Munteanu <florinmunteanu96@gmail.com>
2019-07-21 09:55:31 +0300

    Fix unable to load component

--- src/windows/ProjectPage/ProjectPageView.tsx
+++ src/windows/ProjectPage/ProjectPageView.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { WindowProps } from '../Window';
 
-export class ProjectPageView extends React.Component<WindowProps>{
+export default class ProjectPageView extends React.Component<WindowProps, {}>{
     render(){
-        return <div className="row"> 
-            <div className="col-3">Left Side</div>
-            <div className="col-9">Right Side</div>
+        return  <div className="container-fluid">
+            <div className="row"> 
+                <div className="col-3">Left Side</div>
+                <div className="col-9">Right Side</div>
+            </div>
         </div>
     }
 }
--- src/windows/ProjectPage/ProjectPageWindow.ts
+++ src/windows/ProjectPage/ProjectPageWindow.ts
@@ -2,11 +2,7 @@ import { Window } from "../Window";
 import { Project } from "../../models/Project";
 
 export class ProjectPageWindow extends Window{
-
     constructor(project: Project){
         super(require.resolve('./ProjectPageView'));
-    }
-    public getProps = (): { [key: string]: () => void; } => {
-        return {};
     }
 }
