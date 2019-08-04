0c49c505f6f9c385bf127d005fef1c04ba40d632
Florin Munteanu <florinmunteanu96@gmail.com>
2019-08-04 10:31:31 +0300

    Refactor

--- src/models/Components/GridLayout/ColumnComponent.tsx
+++ src/models/Components/GridLayout/ColumnComponent.tsx
@@ -1,18 +1,11 @@
-import { IComponent } from "../../IComponent";
-import { IProperty } from "../../IProperty";
 import { Component } from "../../Component";
-import * as React from 'react';
 
 export class ColumnComponent extends Component {     
-    children: IComponent[] = [];
-    
-    style = {
-        width: "100px"
-    }
-
-    view = <div>Column</div>;
-
     constructor(){
         super("Column");
+
+        this.style = {
+            width: "100px"
+        }
     }
 }
