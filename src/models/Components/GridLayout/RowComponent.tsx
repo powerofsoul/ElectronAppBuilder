3626c9d83bedd615847b99cd8001b828210e59db
Florin Munteanu <florinmunteanu96@gmail.com>
2019-08-10 12:09:36 +0300

    Remove background property

--- src/models/Components/GridLayout/RowComponent.tsx
+++ src/models/Components/GridLayout/RowComponent.tsx
@@ -1,20 +1,14 @@
-import { NumericProperty } from "../../../models/Properties/NumericProperty";
 import { ColumnComponent } from "./ColumnComponent";
 import { Component } from "../Component";
-import { StringProperty } from "../../Properties/StringProperty";
 
 export class RowComponent extends Component {
     constructor(rowCount = () => 1) {
         super("Row");
-        this.properties = {
-            "BackgroundColor": new StringProperty('Background Color', "transparent")
-        }
         
         this.style= () => { return {
             display: "flex",
             width: '100%',
             height: `${100/rowCount()}%`,
-            backgroundColor: this.properties['BackgroundColor'].value as string
         }}
 
         this.childrenTypes = {
